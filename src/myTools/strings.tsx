5164d31b0dd8ec679438f5c3555d7fcf5e3df64f
Nadi13 <nadia_yudintseva@mail.ru>
2024-01-08 01:00:30 +0600

    сделан header

---
+++ src/myTools/strings.tsx
@@ -0,0 +1,11 @@
+const strings = {
+    T: "T",
+    HealthCare: "HealthCare",
+    Home: "Home",
+    FindDoc: "Find a doctor",
+    Apps: "Apps",
+    Testimonials: "Testimonials",
+    About: "About us"
+};
+
+export default strings;
